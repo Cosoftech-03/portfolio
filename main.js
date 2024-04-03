5dc88cec0ceabe35b77358b36ce3ce1709379eb5
Arvind Krishna Yadav <136746426+Cosoftech-03@users.noreply.github.com>
2024-04-04 00:46:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new typed(".text", {
+  Strings: ["Front-End Developer", "App Developer", "Cloud Assistant"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
